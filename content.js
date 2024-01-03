fe82dce0c42e38f6809d6a374c355649f8d4f130
CenterKeyCooper <cooper.wilk@tagmagnet.org>
2024-01-03 00:18:54 -0600

    skeleton and such

---
+++ content.js
@@ -0,0 +1,7 @@
+document.getElementById('content').innerHTML = `
+    <p>I am a 3rd year computer science student at the University of Texas at Austin.</p>
+`;
+
+document.getElementById('techbio').innerHTML = `
+    <p>Here is my technical bio wow very impressive</p>
+`;
