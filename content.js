4fb813c83ab556d8b5afd6be15679a2e77f9a3ff
CenterKeyCooper <cooper.wilk@tagmagnet.org>
2024-01-03 02:37:18 -0600

    photo album centered with buttons

--- content.js
+++ content.js
@@ -5,3 +5,8 @@ document.getElementById('content').innerHTML = `
 document.getElementById('techbio').innerHTML = `
     <p>Here is my technical bio wow very impressive</p>
 `;
+
+document.getElementById('campcontent').innerHTML = `
+    <p>For three Summers I taght over 100 campers ages 5 - 17 the basics of programming. I taught young campers Scratch and older campers Java with 5 day curriculums the ended in every camper creating their own original game</p>
+    <p>Back in high school, I was scheduled to do the UT Southwestern STARS program for computer science in biomed, but some global pandemic came along and prevented that.</p>
+`;
